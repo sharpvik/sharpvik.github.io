30a2b0b02bb8ed8c7893e8d1557417af8b3148c7
Viktor Rozenko <sharp.vik@gmail.com>
2019-11-19 20:04:29 +0000

    adding the COPY button

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
+/* 
+ * clipboard.js framework (https://clipboardjs.com)
+ * Initialize for button #cp.
+ */
+new ClipboardJS('#cp').on('success', function(e) { e.clearSelection(); });
+
+/* Ensure that input's value is always #ffffff on page (re)load. */
+document.getElementById('hex').value = '#ffffff';
+
+/* Set background-color of #demo and input's value to a random color when
+   #demo is clicked. */
 document.addEventListener('click', function(e) {
-    if (
-        e.target.matches('input') ||
-        e.target.matches('label') ||
-        e.target.matches('section')
-    ) return;
+    if ( !e.target.matches('#demo') ) return;
     let colour = randomHex();
     document.getElementById('hex').value = colour;
-    document.body.style.background = colour;
+    document.getElementById('demo').style.background = colour;
 });
 
+/* Return a random hex colour string. */
 function randomHex() {
     let hexOut = '#';
     for (let i = 0; i < 6; i++)
@@ -16,7 +24,9 @@ function randomHex() {
     return hexOut;
 }
 
+/* Set background-color of #demo when user changes input's value. */
 function setColor() {
-    document.body.style.background = document.getElementById('hex').value;
+    let colour = document.getElementById('hex').value;
+    document.getElementById('demo').style.background = colour;
 }
 
