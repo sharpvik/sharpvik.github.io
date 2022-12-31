f9c3cb6c341a087d1d53dba714342d2b41aff51d
Viktor A. Rozenko Voitenko <sharp.vik@gmail.com>
2022-12-31 01:22:32 +0000

    feat(typer): display header and footer with explanations

--- src/Typer/Class.elm
+++ src/Typer/Class.elm
@@ -10,6 +10,31 @@ bg =
     [ Class.bg, class "typer-bg" ]
 
 
+h1 : List (Attribute msg)
+h1 =
+    [ Class.h1, class "typer-h1" ]
+
+
+h3 : List (Attribute msg)
+h3 =
+    [ Class.h3, class "typer-h3" ]
+
+
+url : Attribute msg
+url =
+    class "typer-url"
+
+
+highlight : Attribute msg
+highlight =
+    class "typer-highlight"
+
+
+info : Attribute msg
+info =
+    class "typer-info"
+
+
 
 -- TEXT
 
--- src/Typer/Link.elm
+++ src/Typer/Link.elm
@@ -4,3 +4,8 @@ module Typer.Link exposing (..)
 words : String
 words =
     "https://gist.githubusercontent.com/deekayen/4148741/raw/98d35708fa344717d8eee15d11987de6c8e26d7d/1-1000.txt"
+
+
+monkeytype : String
+monkeytype =
+    "https://monkeytype.com/"
